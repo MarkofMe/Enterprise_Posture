6fea3a0584648e25205f80422d81962dbb2bbf9d
MarkofMe <mark.leonard1421@gmail.com>
2016-05-05 13:43:40 +0100

    0.01

---
+++ app/src/androidTest/java/jollyroger/enterprise_posture/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jollyroger.enterprise_posture;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
