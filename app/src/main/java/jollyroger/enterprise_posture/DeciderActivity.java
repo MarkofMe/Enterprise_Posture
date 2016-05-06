3664be6fe045fae771d9506e9cc8e57792ec3835
MarkofMe <mark.leonard1421@gmail.com>
2016-05-06 18:51:34 +0100

    0.8

--- app/src/main/java/jollyroger/enterprise_posture/DeciderActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/DeciderActivity.java
@@ -1,7 +1,9 @@
 package jollyroger.enterprise_posture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class DeciderActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class DeciderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decider);
     }
+
+    public void TakeAnotherPicture(View view) {
+    }
+
+    public void AddToNewPatient(View view) {
+        startActivity(new Intent(this, CreatePatientActivity.class));
+    }
+
+    public void AddToExistingPatient(View view) {
+        startActivity(new Intent(this, AddToPatientActivity.class));
+    }
+
 }
