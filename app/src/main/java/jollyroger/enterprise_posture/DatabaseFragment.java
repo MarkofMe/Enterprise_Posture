70aa5a5bea9139ee6688e8990859b93dca8d1bb2
joelwadedaking1337 <p4100378@live.tees.ac.uk>
2016-05-06 20:03:31 -0700

    Not currently working, but getting close

--- app/src/main/java/jollyroger/enterprise_posture/DatabaseFragment.java
+++ app/src/main/java/jollyroger/enterprise_posture/DatabaseFragment.java
@@ -1,11 +1,22 @@
 package jollyroger.enterprise_posture;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import database.DatabaseHandler;
+import database.ListviewCursorAdapter;
+import database.Patient;
 
 
 /**
@@ -13,16 +24,38 @@ import android.view.ViewGroup;
  */
 public class DatabaseFragment extends Fragment {
 
-
     public DatabaseFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_database, container, false);
+
+        DatabaseHandler dbHandler = new DatabaseHandler(getContext());
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male"));
+
+        Cursor cursor = dbHandler.getPatientsTable();
+
+        ListView lv = (ListView) v.findViewById(R.id.listview_patients);
+
+        ListviewCursorAdapter lvAdapter = new ListviewCursorAdapter(getContext(), cursor);
+        lv.setAdapter(lvAdapter);
+
+
+        while(cursor.moveToNext()){
+            String uname = cursor.getString(cursor.getColumnIndex("FirstName"));
+            Log.d(uname, uname);
+        }
+
         return inflater.inflate(R.layout.fragment_database, container, false);
     }
 
