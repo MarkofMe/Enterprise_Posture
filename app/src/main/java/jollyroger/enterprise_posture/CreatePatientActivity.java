ce0ecf5d3e12f7c294434ddc4e9acf18cc19639b
MarkofMe <mark.leonard1421@gmail.com>
2016-05-09 01:09:46 +0100

    0.11

--- app/src/main/java/jollyroger/enterprise_posture/CreatePatientActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/CreatePatientActivity.java
@@ -1,12 +1,20 @@
 package jollyroger.enterprise_posture;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Date;
+
+import database.DatabaseHandler;
+import database.Patient;
 
 import static jollyroger.enterprise_posture.R.id.create_patient_toolbar;
 
 public class CreatePatientActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +26,12 @@ public class CreatePatientActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void Add(View view)
+    {
+
+    }
+
+
+
 }
