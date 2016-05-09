3e895a9c73e4399d0b9e3c1f8e70de8ee9015523
MarkofMe <mark.leonard1421@gmail.com>
2016-05-09 19:55:26 +0100

    0.12.4.7

--- app/src/main/java/jollyroger/enterprise_posture/CameraActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/CameraActivity.java
@@ -5,12 +5,89 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import static jollyroger.enterprise_posture.R.id.camera_toolbar;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CameraActivity extends AppCompatActivity {
 
+
+    private Camera camera;
+    private Preview preview;
+    private Button captureButton;
+    private static String imageUri;
+
+    private Camera.PictureCallback pic = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null){
+                Log.d(null, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(null, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(null, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    //without the ability to test, I think this SHOULD be the place that the activity changes?
+    public void confirm(){
+        final Context cont = this;
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+
+        b.setMessage(R.string.dialog_message);
+        b.setTitle(R.string.dialog_title);
+
+        //User confirms
+        b.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                camera.takePicture(null, null, pic);
+
+                Intent intent = new Intent(cont, PlotterActivity.class);
+                intent.putExtra("image location as path", imageUri);
+                startActivity(intent);
+            }
+        });
+
+        b.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                return;
+            }
+        });
+
+        AlertDialog ad = b.create();
+        ad.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +98,20 @@ public class CameraActivity extends AppCompatActivity {
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        camera = getCameraInstance();
+        preview = new Preview(this, camera);
+
+        FrameLayout fl = (FrameLayout) findViewById(R.id.camera_preview);
+        fl.addView(preview);
+
+        captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                confirm();
+            }
+        });
 
     }
 
@@ -35,8 +126,60 @@ public class CameraActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        releaseCamera();
+    }
+
+    public static Camera getCameraInstance()
+    {
+        Camera c = null;
+
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e) {
+            Log.d(null, "Error opening camera: " + e.getMessage());
+        }
+
+        return c;
+    }
+
+    private void releaseCamera(){
+        if (camera != null){
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private static File getOutputMediaFile()
+    {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        imageUri = mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg";
+        File mediaFile = new File(imageUri);
+
+        return mediaFile;
     }
 }
---
+++ app/src/main/java/jollyroger/enterprise_posture/Preview.java
@@ -0,0 +1,49 @@
+package jollyroger.enterprise_posture;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by p4071825 on 08/05/16.
+ */
+public class Preview extends SurfaceView implements SurfaceHolder.Callback
+{
+    private SurfaceHolder sh;
+    private Camera camera;
+
+
+    Preview(Context con, Camera c)
+    {
+        super(con);
+        camera = c;
+        sh = getHolder();
+        sh.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }
+        catch(Exception e){
+            Log.d(null, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+
+    }
+}
