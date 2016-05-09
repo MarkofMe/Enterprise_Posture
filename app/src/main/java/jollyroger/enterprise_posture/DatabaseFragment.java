fdbfc420c7e7ae11ab326e660e89f0f5d4bf835d
MarkofMe <mark.leonard1421@gmail.com>
2016-05-09 16:19:57 +0100

    0.12.1

--- app/src/main/java/jollyroger/enterprise_posture/DatabaseFragment.java
+++ app/src/main/java/jollyroger/enterprise_posture/DatabaseFragment.java
@@ -36,10 +36,6 @@ public class DatabaseFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_database, container, false);
 
-//        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male", true));
-//        dbHandler.insertDataPatients(new Patient("Mark", "wade", new Date("15/11/1992"), "male", true));
-//        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("15/11/1992"), "male", true));
-
         populateListView(v);
 
         return v;
@@ -49,7 +45,7 @@ public class DatabaseFragment extends Fragment {
 
         DatabaseHandler dbHandler = new DatabaseHandler(getContext());
 
-        dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("11/15/1992"), "male", 1));
+        //dbHandler.insertDataPatients(new Patient("joel", "wade", new Date("11/15/1992"), "male", 1));
 
         Cursor cursor = dbHandler.getPatientsTable();
 
