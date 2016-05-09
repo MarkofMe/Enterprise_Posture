ff490988fcbee5af0673577fe789339b53c2b050
joelwadedaking1337 <p4100378@live.tees.ac.uk>
2016-05-09 13:54:16 -0700

    First and surname fields must be filled in to add patient to db

--- app/src/main/java/jollyroger/enterprise_posture/CreatePatientActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/CreatePatientActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Date;
 
@@ -36,12 +37,20 @@ public class CreatePatientActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    //On button press of add patient
     public void Add(View view)
     {
+        //Grab text from the text fields
         EditText firstName = (EditText) findViewById(R.id.firstNameInput);
         EditText lastName = (EditText) findViewById(R.id.lastNameInput);
-        dbHandler.insertDataPatients(new Patient(firstName.getText().toString(), lastName.getText().toString(), new Date(date), "male", 1));
-        finish();
+
+        //If both the fore and sur name fields contain text. (currently only needs text, doesnt need actual names i.e can have only spaces/punctuation.
+        if (!firstName.getText().toString().matches("") && !lastName.getText().toString().matches("")){
+            dbHandler.insertDataPatients(new Patient(firstName.getText().toString(), lastName.getText().toString(), new Date(date), "male", 1));
+            finish();
+        } else { //One of the fields was blank.
+            Toast.makeText(this, "You did not enter your name", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
