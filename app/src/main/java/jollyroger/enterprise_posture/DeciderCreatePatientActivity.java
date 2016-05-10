fcb2c11ca8651620c19bb95d56c73478b1da5688
MarkofMe <mark.leonard1421@gmail.com>
2016-05-10 23:19:09 +0100

    0.14.6.2

--- app/src/main/java/jollyroger/enterprise_posture/DeciderCreatePatientActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/DeciderCreatePatientActivity.java
@@ -1,13 +1,117 @@
 package jollyroger.enterprise_posture;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DeciderCreatePatientActivity extends AppCompatActivity {
+import java.util.Date;
+
+import database.DatabaseHandler;
+import database.Patient;
+
+import static jollyroger.enterprise_posture.R.id.create_patient_toolbar;
+
+public class DeciderCreatePatientActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    DatabaseHandler dbHandler = new DatabaseHandler(this);
+    String firstName;
+    String lastName;
+    int date;
+
+
+    int month_x, day_x;
+    int year_x = 2000;
+    static final int DIALOG_ID = 0;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decider_create_patient);
+
+        Toolbar toolbar = (Toolbar) findViewById(create_patient_toolbar);
+        setSupportActionBar(toolbar);
+
+        showDialogOnButtonClick();
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner dropdown = (Spinner)findViewById(R.id.decidergenderSpinner);
+        String[] items = new String[]{"Male", "Female", "Other", "Not specified"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+    }
+
+    public void showDialogOnButtonClick() {
+        btn = (Button) findViewById(R.id.decideraddPatientDOB);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID) {
+            return new DatePickerDialog(this, dpickerListener, year_x, month_x, day_x);
+        } else {
+            return null;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear + 1;
+            day_x = dayOfMonth;
+            TextView DOB = (TextView) findViewById(R.id.deciderDOB);
+            DOB.setText("Date of Birth: " + day_x + "/" + month_x + "/" + year_x);
+
+        }
+    };
+
+    //On button press of add patient
+    public void Add(View view) {
+        //Grab text from the text fields
+        EditText firstName = (EditText) findViewById(R.id.deciderfirstNameInput);
+        EditText lastName = (EditText) findViewById(R.id.deciderlastNameInput);
+        Spinner gender = (Spinner) findViewById(R.id.decidergenderSpinner);
+
+        Log.d("Gender", gender.getSelectedItem().toString());
+
+        //If both the fore and sur name fields contain text. (currently only needs text, doesnt need actual names i.e can have only spaces/punctuation.
+        if (!firstName.getText().toString().matches("") && !lastName.getText().toString().matches("")) {
+            dbHandler.insertDataPatients(new Patient(firstName.getText().toString(), lastName.getText().toString(), new Date(year_x - 1900, month_x, day_x), gender.getSelectedItem().toString(), 1));
+            finish();
+        } else { //One of the fields was blank.
+            Toast.makeText(this, "You did not enter your name", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
