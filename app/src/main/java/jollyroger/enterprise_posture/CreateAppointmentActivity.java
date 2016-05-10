7300cdbda7855a31136139ecd0d850d143f89b5a
MarkofMe <mark.leonard1421@gmail.com>
2016-05-10 23:37:02 +0100

    0.14.6.4

---
+++ app/src/main/java/jollyroger/enterprise_posture/CreateAppointmentActivity.java
@@ -0,0 +1,13 @@
+package jollyroger.enterprise_posture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_appointment);
+    }
+}
