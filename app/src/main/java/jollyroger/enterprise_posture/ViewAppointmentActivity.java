da25d16e79b0f34a1f5de282b3f169bcf3caddd8
joelwadedaking1337 <p4100378@live.tees.ac.uk>
2016-05-14 17:38:39 -0700

    Fixes

--- app/src/main/java/jollyroger/enterprise_posture/ViewAppointmentActivity.java
+++ app/src/main/java/jollyroger/enterprise_posture/ViewAppointmentActivity.java
@@ -3,6 +3,7 @@ package jollyroger.enterprise_posture;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -41,18 +42,18 @@ public class ViewAppointmentActivity extends AppCompatActivity {
 
         Bundle data = getIntent().getExtras();
         Patient p = data.getParcelable("patient");
-        Appointment a = new Appointment();
+        //Appointment a = new Appointment();
 
         //For mark, the cursor contains all the appointments for the patient p.
         DatabaseHandler db = new DatabaseHandler(getBaseContext());
         Cursor c = db.getPatientAppointments(p.getPatientID());
 
         //Use this to grab specific columns, e.g.
-        String appDate = c.getString(c.getColumnIndexOrThrow("AppointmentDate"));
+        //String appDate = c.getString(c.getColumnIndexOrThrow("AppointmentDate"));
         //Gets the appDate as a String (duh). Take the column name e.g "AppointmentDate" from the dbhandler at the top.
 
         Log.d("PatientID: ", "" + p.getPatientID());
-        Log.d("PatientID: ", "" + a.getPatientID());
+        //Log.d("PatientID: ", "" + a.getPatientID());
         TextView appointmentNumber = (TextView) findViewById(R.id.viewAppointmentNumber);
         TextView appointmentDate = (TextView) findViewById(R.id.viewAppointmentDate);
         TextView postureResult = (TextView) findViewById(R.id.viewPostureResult);
@@ -60,12 +61,15 @@ public class ViewAppointmentActivity extends AppCompatActivity {
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 
-        appointmentNumber.setText(c.getString(c.getColumnIndexOrThrow("AppointmentNo")));
-        appointmentDate.setText(formatter.format(c.getString(c.getColumnIndexOrThrow("AppointmentDate"))));
-        postureResult.setText(formatter.format(c.getColumnIndexOrThrow("Diagnostic")));
-        byte[] b = c.getBlob(c.getColumnIndexOrThrow("PatientImage"));
-        Bitmap img = BitmapFactory.decodeByteArray(b,0, b.length);
-        appointment_Imageholder.setImageBitmap(img);
+        Log.d("App number", (DatabaseUtils.dumpCursorToString(c)));
+        //Log.d("App number", c.getString(c.getColumnIndexOrThrow("AppointmentNo")));
+
+//        appointmentNumber.setText(c.getString(c.getColumnIndexOrThrow("AppointmentNo")));
+//        appointmentDate.setText(formatter.format(c.getString(c.getColumnIndexOrThrow("AppointmentDate"))));
+//        postureResult.setText(formatter.format(c.getColumnIndexOrThrow("Diagnostic")));
+//        byte[] b = c.getBlob(c.getColumnIndexOrThrow("PatientImage"));
+//        Bitmap img = BitmapFactory.decodeByteArray(b,0, b.length);
+//        appointment_Imageholder.setImageBitmap(img);
     }
 
     public void EditAppointment(View view) {
